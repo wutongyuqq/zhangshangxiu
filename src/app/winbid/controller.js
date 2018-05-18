585416307a759fa1c0de7fe5b0170ef8eedbffa7
peng li <314421674@qq.com>
2018-05-18 17:10:57 +0800

    新增页面提交

--- src/app/winbid/controller.js
+++ src/app/winbid/controller.js
@@ -1,18 +1,5 @@
-app.controller('WinBidCtrl', ['$http', '$uibModal', '$log', '$scope', '$state', function ($http, $uibModal, $log, $scope, $state) {
+app.controller('WinBidCtrl', ['$http', '$scope', '$state', function ($http, $scope, $state) {
 
-    var selt = this;
-    var paramsPage = {
-        pageNo: 1,
-        pbModes: '',
-        type: 0,
-        kbDateStart: '',
-        projSumStart: '',
-        projSumEnd: '',
-        pageSize: 20,
-        kbDateEnd: '',
-        regions: '',
-        zzType: '',
-        projectType: ''
-    };
+
 
 }]);

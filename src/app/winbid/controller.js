6626cb15d7a66e46141112ce637c5155da85b681
zourong <1298452397@qq.com>
2018-05-26 17:02:01 +0800

    项目创建提交

--- src/app/winbid/controller.js
+++ src/app/winbid/controller.js
@@ -44,43 +44,33 @@ app.controller('WinBidCtrl', ['$http', '$scope', '$state','locals', function ($h
 }]);
 app.controller('WinTotalCtrl', ['$http', '$scope', '$state','locals', function ($http, $scope, $state,locals) {
     var carInfo =locals.getObject("carInfo");
-
+    var jsd_id = locals.get("jsd_id");
+    var ticheTime = locals.get("ticheTime");
     $scope.carInfo = carInfo;
+    $scope.jsd_id = jsd_id;
+    $scope.ticheTime = ticheTime;
     var params = {
         db:"mycon1",
-        function:"sp_fun_down_poundage"
+        function:"sp_fun_down_repair_list_main",
+        jsd_id:jsd_id
     };
-    var jsToBean = new Object();
-    jsToBean.wxFl="";
-    jsToBean.zfbFl="";
-    jsToBean.yhkFl="";
-    $scope.jsToBean = jsToBean;
+    var jsonStr =  angular.toJson(params);
+    var gdData = new Object();
+    $scope.gdData = gdData;
     $http({
         method: 'post',
         url: '/restful/pro',
         dataType: "json",
-        data: angular.toJson(params),
+        data:jsonStr
     }).success(function (data, status, headers, config) {
         console.log(data);
         var state = data.state;
         if (state == 'ok') {
-            var dataArray = data.data;
-            if(dataArray!=null && dataArray.length>0){
-                var jsdBean = jsToBean;
-
-                for(var i=0;i<dataArray.length;i++){
-                    var bean = dataArray[i];
-                    if(bean.name=="微信"){
-                        jsdBean.wxFl = bean.setup2;
-                    }else if(bean.name=="支付宝"){
-                        jsdBean.zfbFl = bean.setup2;
-                    }else if(bean.name=="中行刷卡"){
-                        jsdBean.yhkFl = bean.setup2;
-                    }
-                }
-                $scope.jsToBean = jsdBean;
+            var gdDataList = data.data;
+            if(gdDataList!=null&&gdDataList.length>0){
+                 gdData = gdDataList[0];
+                $scope.gdData = gdData;
             }
-            console.log(data.msg);
         }
     });
 

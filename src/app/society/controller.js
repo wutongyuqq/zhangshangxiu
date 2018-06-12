e13bf5588cf4fa5213713090dd29a9d7b0d8a775
wutongyuqq <314421674@qq.com>
2018-06-13 07:01:09 +0800

    增加仿苹果时间控件

--- src/app/society/controller.js
+++ src/app/society/controller.js
@@ -2,10 +2,10 @@ app.controller('carInfoCtrl', ['$http', '$scope', '$state', "locals", "ionicToas
     $scope.getCarInfoData = function () {
         var carInfo = locals.getObject("carInfo");
         var params = {
-            db: "mycon1",
-            function: "sp_fun_down_car_owner",
+            db:"mycon1",
+            function:"sp_fun_down_car_info",
             customer_id: carInfo.customer_id
-        };
+    };
         var jsonStr = angular.toJson(params);
         $http({
             method: 'post',
@@ -17,7 +17,8 @@ app.controller('carInfoCtrl', ['$http', '$scope', '$state', "locals", "ionicToas
             var state = data.state;
 
             if (state == 'ok') {
-
+                var dataArr =  data.data;
+                $scope.carInfo = dataArr[0];
             }
 
         }).error(function (data) {
@@ -29,7 +30,7 @@ app.controller('carInfoCtrl', ['$http', '$scope', '$state', "locals", "ionicToas
 
 
 app.controller('czInfoCtrl', ['$http', '$scope', '$state', "locals", "ionicToast", function ($http, $scope, $state, locals, ionicToast) {
-    $scope.getCarInfoData = function () {
+    $scope.getCarOwerData = function () {
         var carInfo = locals.getObject("carInfo");
         var params = {
             db: "mycon1",
@@ -45,15 +46,13 @@ app.controller('czInfoCtrl', ['$http', '$scope', '$state', "locals", "ionicToast
         }).success(function (data, status, headers, config) {
             console.log(data);
             var state = data.state;
-
             if (state == 'ok') {
                 var dataArr =  data.data;
                 $scope.czInfo = dataArr[0];
             }
-
         }).error(function (data) {
             ionicToast.show("服务异常", "middle", 2000);
         });
     }
-    $scope.getCarInfoData();
+    $scope.getCarOwerData();
 }]);
